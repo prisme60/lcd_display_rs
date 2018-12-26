26536a21730020bd11e414260ecdedc513a72e62
Christian FOUCHER <christian.foucher@gmail.com>
2018-12-26 22:18:28 +0100

    Use LCD display

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use std::time::Duration;
-use std::thread::sleep;
-use lcd_rgb_keypad::{lcd::{commands, glyphes}, leds::{set, Leds}};
+use std::{
+    time::Duration,
+    thread::sleep
+};
+use lcd_rgb_keypad::{
+    lcd::{
+        Lcd,
+        commands
+    },
+    leds::{
+        set,
+        Leds}
+};
 
 fn main() {
     println!("Enter :");
+
+    // Test leds
     set(Leds::RED);
     sleep(Duration::from_secs(1));
     set(Leds::GREEN);
@@ -11,5 +23,33 @@ fn main() {
     set(Leds::BLUE);
     sleep(Duration::from_secs(1));
     set(Leds::empty());
+
+    //Test LCD
+    let mut l = Lcd::new("/dev/lcd");
+    l.append_raw_str(commands::DISPLAY_ON);
+    l.append_raw_str(commands::BACKLIGHT_ON);
+    l.append_str("Coucou Héro\ndes temps héroiques\n");
+    l.apply();
+    sleep(Duration::from_secs(1));
+
+    l.append_raw_str(commands::BACKLIGHT_FLASH);
+    l.append_str("Au revoir mon héro\nJoyeux Noël\n");
+    l.apply();
+    sleep(Duration::from_millis(500));
+
+    for _i in 0..16 {
+        l.append_raw_str(commands::SHIFT_DISPLAY_LEFT);
+        l.apply();
+        sleep(Duration::from_millis(500));
+    }
+
+    l.append_raw_str(commands::DISPLAY_OFF);
+    l.append_raw_str(commands::BACKLIGHT_OFF);
+    l.apply();
+
+    //Test KeyBoard
+
+
+
     println!("Exit :");
 }
