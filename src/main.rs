7deee640f43777a65b42865fc8d37b6683e23726
Christian FOUCHER <christian.foucher@gmail.com>
2018-12-27 13:59:52 +0100

    Use Rustbox in order to capture keyboard events (arrows and enter)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use std::{
-    time::Duration,
-    thread::sleep
-};
 use lcd_rgb_keypad::{
-    lcd::{
-        Lcd,
-        commands
-    },
-    leds::{
-        set,
-        Leds}
+    lcd::{commands, Lcd},
+    leds::{set, Leds},
 };
+use rustbox::{Color, Key, RustBox};
+use std::{error::Error, thread::sleep, time::Duration};
 
 fn main() {
     println!("Enter :");
@@ -26,7 +19,9 @@ fn main() {
 
     //Test LCD
     let mut l = Lcd::new("/dev/lcd");
+    l.append_raw_str(commands::REINITIALISE);
     l.append_raw_str(commands::DISPLAY_ON);
+    l.append_raw_str(commands::CURSOR_OFF);
     l.append_raw_str(commands::BACKLIGHT_ON);
     l.append_str("Coucou Héro\ndes temps héroiques\n");
     l.apply();
@@ -48,8 +43,50 @@ fn main() {
     l.apply();
 
     //Test KeyBoard
+    let rustbox = match RustBox::init(Default::default()) {
+        Result::Ok(v) => v,
+        Result::Err(e) => panic!("{}", e),
+    };
 
-
+    rustbox.print(
+        1,
+        1,
+        rustbox::RB_BOLD,
+        Color::White,
+        Color::Black,
+        "Hello, world!",
+    );
+    rustbox.print(
+        1,
+        3,
+        rustbox::RB_BOLD,
+        Color::White,
+        Color::Black,
+        "Push buttons on the keypad",
+    );
+    rustbox.present();
+    loop {
+        rustbox.print(
+            1,
+            6,
+            rustbox::RB_BOLD,
+            Color::Black,
+            Color::White,
+            match rustbox.poll_event(false) {
+                Ok(rustbox::Event::KeyEvent(key)) => match key {
+                    Key::Enter => "Enter",
+                    Key::Right => "->   ",
+                    Key::Left => "<-   ",
+                    Key::Up => "Up   ",
+                    Key::Down => "Down ",
+                    _ => break,
+                },
+                Err(e) => panic!("{}", e.description()),
+                _ => "     ",
+            },
+        );
+        rustbox.present();
+    }
 
     println!("Exit :");
 }
